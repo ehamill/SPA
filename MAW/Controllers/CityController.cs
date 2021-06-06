8797d9419cee64a8c1e3859de5da31beaf0c1612
eric hamill <erichamill@gmail.com>
2021-06-05 19:48:37 -0500

    asfd

--- MAW/Controllers/CityController.cs
+++ MAW/Controllers/CityController.cs
@@ -81,34 +81,7 @@ namespace MAW.Controllers
             //var parameters = ClientRequestParametersProvider.GetClientParameters(HttpContext, clientId);
             return UserCity;
         }
-        [HttpGet]
-        public async Task<JsonResult> Get()
-        { //testinggnsls dfjasdfa jf
-            string ServerId = "SS1";
-            City UserCity = await _context.City.Where(c => c.ServerId == ServerId).FirstOrDefaultAsync();
-            if (UserCity == null)
-            {
-                UserCity = new City()
-                {
-                    CityId = 1,
-                    CityName = "FirstCityName",
-                    ServerId = "SS1",
-                    UserId = "asdjfl2234"
-                };
-            }
-            //var parameters = ClientRequestParametersProvider.GetClientParameters(HttpContext, clientId);
-            return new JsonResult(new { city = UserCity });
-        }
-
-        //[HttpGet]
-        [HttpGet("{ServerId}")]
-        public async Task<City> Get(string ServerId)
-        {
-            //string UserId = User.Identity.Name;
-            City UserCity = await _context.City.Where(c => c.ServerId == ServerId).FirstOrDefaultAsync();
-            //var parameters = ClientRequestParametersProvider.GetClientParameters(HttpContext, clientId);
-            return UserCity;
-        }
+        
     }
 
     
